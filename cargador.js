d95fb3684f64e1f8071adeb6c1423dbcd402677d
Dinfcs <157985276+Dinfcs@users.noreply.github.com>
2025-03-30 13:22:52 -0500

    Update cargador.js

--- cargador.js
+++ cargador.js
@@ -11,36 +11,31 @@
 (async function() {
     'use strict';
 
+    // Crear una política de Trusted Types
+    const trustedPolicy = window.trustedTypes?.createPolicy('default', {
+        createScriptURL: (url) => url
+    });
+
     const scripts = [
-        { urlPattern: /^https:\/\/docs\.google\.com\/forms\/.*1FAIpQLSeERJOjmc-5ubYtuxSk7xD1IHKGRl_jfGNHbM3JB1KqaZ9ISw.*/, scriptUrl: 'https://dinfcs.github.io/SISVAN/AutoFillsISVAN1.js' },
-        { urlPattern: /^https:\/\/docs\.google\.com\/forms\/.*1FAIpQLScPb-tduoYfOTZ1nDhZaDwQPENA315YOXdTYdpjIrUoG7lp-Q.*/, scriptUrl: 'https://dinfcs.github.io/SISVAN/AutoFillSISVAN2.js' }
-        
+        { urlPattern: /1FAIpQLSeERJOjmc-5ubYtuxSk7xD1IHKGRl_jfGNHbM3JB1KqaZ9ISw/, scriptUrl: 'https://dinfcs.github.io/SISVAN/AutoFillsISVAN1.js' },
+        { urlPattern: /1FAIpQLScPb-tduoYfOTZ1nDhZaDwQPENA315YOXdTYdpjIrUoG7lp-Q/, scriptUrl: 'https://dinfcs.github.io/SISVAN/AutoFillSISVAN2.js' }
     ];
 
     const loadScript = (scriptUrl) => {
-        return new Promise((resolve, reject) => {
-            const script = document.createElement('script');
-            script.src = scriptUrl;
-            script.onload = () => {
-                console.log(`Script loaded: ${scriptUrl}`);
-                resolve();
-            };
-            script.onerror = () => {
-                console.error(`Error loading script: ${scriptUrl}`);
-                reject();
-            };
-            document.head.appendChild(script);
-        });
+        const script = document.createElement('script');
+        script.src = trustedPolicy ? trustedPolicy.createScriptURL(scriptUrl) : scriptUrl;
+        script.onload = () => console.log(`Script loaded: ${scriptUrl}`);
+        script.onerror = () => console.error(`Error loading script: ${scriptUrl}`);
+        document.head.appendChild(script);
     };
 
     try {
-        const matchingScripts = scripts.filter(({ urlPattern }) => urlPattern.test(window.location.href));
-        for (const { scriptUrl } of matchingScripts) {
-            await loadScript(scriptUrl);
-        }
-        
+        scripts
+            .filter(({ urlPattern }) => urlPattern.test(window.location.href))
+            .forEach(({ scriptUrl }) => loadScript(scriptUrl));
+
         // Cargar el script UniversalAE.js al final
-        await loadScript('https://dinfcs.github.io/Deckardaov/DeckardScripts/ExternalScripts/UniversalAE.js');
+        loadScript('https://dinfcs.github.io/Deckardaov/DeckardScripts/ExternalScripts/UniversalAE.js');
     } catch (error) {
         console.error('Error loading scripts:', error);
     }
