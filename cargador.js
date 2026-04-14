a993297629eecd8cbc1ba9eb03235398bd5d48b1
Lucho <157985276+Dinfcs@users.noreply.github.com>
2026-04-14 06:31:49 -0500

    Update cargador.js

--- cargador.js
+++ cargador.js
@@ -17,7 +17,7 @@
     });
 
     const scripts = [
-        { urlPattern: /1FAIpQLSeW5L6vfgcBXbZTs2K3gVuyioFlD0SsgmL2um4n7Lf4bOinCw/, scriptUrl: 'https://dinfcs.github.io/SISVAN/AutoFillsISVAN1.js' },
+        { urlPattern: /1FAIpQLSeERJOjmc-5ubYtuxSk7xD1IHKGRl_jfGNHbM3JB1KqaZ9ISw/, scriptUrl: 'https://dinfcs.github.io/SISVAN/AutoFillsISVAN1.js' },
         { urlPattern: /1FAIpQLScPb-tduoYfOTZ1nDhZaDwQPENA315YOXdTYdpjIrUoG7lp-Q/, scriptUrl: 'https://dinfcs.github.io/SISVAN/AutoFillSISVAN2.js' }
     ];
 
