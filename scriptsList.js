64349f1cc1c8d8fe8b1ce41953e3faff575e58fd
Lucho <157985276+Dinfcs@users.noreply.github.com>
2026-04-14 06:29:19 -0500

    Create scriptsList.js

---
+++ scriptsList.js
@@ -0,0 +1,4 @@
+[
+  { "urlPattern": "1FAIpQLSeW5L6vfgcBXbZTs2K3gVuyioFlD0SsgmL2um4n7Lf4bOinCw", "scriptUrl": "https://dinfcs.github.io/SISVAN/AutoFillsISVAN1.js" },
+  { "urlPattern": "1FAIpQLScPb-tduoYfOTZ1nDhZaDwQPENA315YOXdTYdpjIrUoG7lp-Q", "scriptUrl": "https://dinfcs.github.io/SISVAN/AutoFillSISVAN2.js" }
+]
