b45402ba46012c3cbeccd6fb6e5c262777a82008
Lucho <157985276+Dinfcs@users.noreply.github.com>
2026-04-14 06:38:21 -0500

    Update scriptsList.js

--- scriptsList.js
+++ scriptsList.js
@@ -1,5 +1,5 @@
 [
  { urlPattern: /1FAIpQLSeERJOjmc-5ubYtuxSk7xD1IHKGRl_jfGNHbM3JB1KqaZ9ISw/, scriptUrl: 'https://dinfcs.github.io/SISVAN/AutoFillsISVAN1.js' },
  { urlPattern: /1FAIpQLScPb-tduoYfOTZ1nDhZaDwQPENA315YOXdTYdpjIrUoG7lp-Q/, scriptUrl: 'https://dinfcs.github.io/SISVAN/AutoFillSISVAN2.js' },
-  { urlPattern: /1FAIpQLSeW5L6vfgcBXbZTs2K3gVuyioFlD0SsgmL2um4n7Lf4bOinCw/, scriptUrl: 'https://dinfcs.github.io/SISVAN/autofillsisvan3.js' }
+  { urlPattern: /forms/d/e/1FAIpQLSeW5L6vfgcBXbZTs2K3gVuyioFlD0SsgmL2um4n7Lf4bOinCw, scriptUrl: 'https://dinfcs.github.io/SISVAN/autofillsisvan3.js' }
 ]
